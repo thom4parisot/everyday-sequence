d560dd1bb29409f9cec750553346d08d9168c7e2
oncletom <hi@oncletom.io>
2015-12-28 10:23:33 +0100

    User defined image dimension
    Update fountayne road thumb

--- index.js
+++ index.js
@@ -16,19 +16,20 @@ window.addEventListener('load', function() {
 
 function renderCollectionActions(container, collections, renderers){
   var collectionKeys = Object.keys(collections);
+  var optionsEl = document.querySelector('.options');
 
   Object.keys(renderers).forEach(function(rendererKey) {
     var renderer = renderers[rendererKey];
 
-    container.appendChild(h('dt.renderer', rendererKey));
+    var el = container.insertBefore(h('dt.renderer', rendererKey), optionsEl);
 
     Object.keys(collections).forEach(function(collectionId){
       var onClick = renderer.bind(null, collections[collectionId], 'tmp/' + collectionId);
-      
-      container.appendChild(h(
+
+      container.insertBefore(h(
         'dd.collection',
         h('button', { 'onclick': onClick }, collectionId))
-      );
+      , el.nextSibling);
     });
   });
 }
--- src/renderers/sequence-faded.js
+++ src/renderers/sequence-faded.js
@@ -5,21 +5,29 @@ var paper = require('paper');
 var reverse = require('alpha-sort').desc;
 var Loader = require('../loader');
 
-paper.setup(document.querySelector('canvas'));
-
 var Raster = paper.Raster;
 var Size = paper.Size;
 var Path = paper.Path;
 var Group = paper.Group;
 
-module.exports = function(collection, collectionPath) {
+module.exports = function(collection, collectionPath, event) {
   var count = collection.images.length;
   var scale = 3;
+  var imageWidth = parseFloat(document.querySelector('input[name="width"]').value) || 600;
 
+  var canvas = document.querySelector('canvas');
+  canvas.height = imageWidth;
+  canvas.width = imageWidth;
+  canvas.style.height = 'auto';
+  canvas.style.width = 'auto';
+
+
+  paper.setup(document.querySelector('canvas'));
   paper.project.clear();
+  console.log(imageWidth, canvas.height);
 
   each(collection.images.sort(reverse), function(imagePath, i, next) {
-    var raster = new Raster(collectionPath + '/' + imagePath);
+    var raster = new Raster(collectionPath + '/' + imagePath, paper.view.center);
 
     raster.onLoad = function(){
       Loader.update(i / count * 100);
@@ -27,7 +35,7 @@ module.exports = function(collection, collectionPath) {
       // raster.blendMode = 'overlay';
       raster.opacity = 1 - (i / count);
 
-      raster.size = new Size(600, 600);
+      raster.size = new Size(imageWidth, imageWidth);
       raster.fitBounds(paper.view.bounds);
 
       var rectangle = new Path.Rectangle(
